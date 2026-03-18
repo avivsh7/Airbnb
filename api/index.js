c70028556175280597360cbe68973580c62d4403
Aviv Shoshan <avivsh7@gmail.com>
2026-03-18 20:07:47 +0200

    Updated CORS origin for production deployment.

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: 'https://main.d128oouv6upg74.amplifyapp.com',
 }));
 mongoose.connect(process.env.MONGO_URL);
 
