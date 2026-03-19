b69ec099d0f19e365932d0ed9881dd9bb400a4bf
Aviv Shoshan <avivsh7@gmail.com>
2026-03-19 12:41:33 +0200

    Adapted upload-by-link endpoint for production env.

--- api/index.js
+++ api/index.js
@@ -107,13 +107,17 @@ app.post('/logout', (req, res) => {
 });
 
 app.post('/upload-by-link', async (req,res) => {
-    const {link} = req.body;
-    const newName = 'photo' + Date.now() + '.jpg';
-    await imageDownloader.image({
-        url: link,
-        dest: __dirname + '/uploads/' +newName,
-    });
-    res.json(newName);
+    try {
+        const {link} = req.body;
+        const newName = 'photo' + Date.now() + '.jpg';
+        await imageDownloader.image({
+            url: link,
+            dest: __dirname + '/uploads/' + newName,
+        });
+        res.json(newName);
+    } catch (e) {
+        res.status(500).json(e.message);
+    }
 });
 
 const photosMiddleware = multer({dest:'uploads/'});
